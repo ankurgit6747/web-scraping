f826f580f907f9d505462f07d9b5eb8ce96d9e06
ankurgit6747 <ankur02srma@gmail.com>
2024-01-11 00:19:52 +0530

    create two api with fast api to get scId and get financials

--- python_table_to_CSV_file/main.py
+++ python_table_to_CSV_file/main.py
@@ -1,20 +1,27 @@
 from fastapi import FastAPI
 import requests
-from typing import Optional
-
+from bs4 import BeautifulSoup # parsing HTML nad XML documents
 
 app = FastAPI()
 
-@app.get("/get_sc_id/{keyword}")
-def get_sc_id(keyword: str):
-    response = requests.get(f"https://api.moneycontrol.com/mcapi/v1/fys/api/v1/search?keyword={keyword}")
-    print(">>>>>>>>>>>>>>>", response.text)
+@app.get("/get_scId/{keyword}")
+def get_scId_from_api(keyword: str):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    }
+    response = requests.get(f"https://api.moneycontrol.com/mcapi/v1/fys/api/v1/search?keyword={keyword}", headers=headers)
     data = response.json()
-    sc_id = data['data'][0]['scId']  # 'scId' is inside the 'data' key
-    return {"sc_id": sc_id}
+    
+    if data["data"]:
+        return {"scId": data["data"][0]["scId"]}
+    else:
+        return {"error": "No data found for this keyword"}
 
 @app.get("/get_financials/{sc_id}")
 def get_financials(sc_id: str):
-    response = requests.get(f"https://www.moneycontrol.com/stocks/company_info/print_financials.php?sc_did={sc_id}&type=balance_VI")
-    # Process the response as needed
-    return response.content
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    }
+    response = requests.get(f"https://www.moneycontrol.com/stocks/company_info/print_financials.php?sc_did={sc_id}&type=balance_VI", headers=headers)
+    return {"content": response.content.decode('utf-8', 'ignore')}
+
