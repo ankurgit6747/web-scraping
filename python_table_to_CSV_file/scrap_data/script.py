3a297e30c080d077007365caefad45e4ba59757b
ankur <ankur02srma@gmail.com>
2024-01-18 14:25:53 +0530

    fix csv file

--- python_table_to_CSV_file/scrap_data/script.py
+++ python_table_to_CSV_file/scrap_data/script.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-url = 'https://www.moneycontrol.com/stocks/company_info/print_financials.php?sc_did=SE17&type=balance_VI'
+url = 'https://www.moneycontrol.com/stocks/company_info/print_financials.php?sc_did=IT&type=balance_VI'
 
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
@@ -10,9 +10,12 @@ table = soup.find_all('table')[3]
 
 data = []
 for row in table.find_all('tr'):
+    if row.find('img'):
+        data.append([None]*len(cols))
+        continue
     cols = row.find_all('td')
-    cols = [ele.text.strip() for ele in cols]
-    data.append([ele for ele in cols if ele])   
+    cols = [ele.text.strip() if ele.text.strip() != '' else None for ele in cols]
+    data.append(cols)   
 
 df = pd.DataFrame(data)
 df.to_csv('output.csv', index=False, header=False)
